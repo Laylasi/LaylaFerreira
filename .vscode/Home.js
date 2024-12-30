e89d62ec8d91ffc811178ac21039e7360297fd7b
Laylasi <157524500+Laylasi@users.noreply.github.com>
2024-12-30 00:42:47 -0500

    Updated
    This includes the gradient

--- .vscode/Home.js
+++ .vscode/Home.js
@@ -1,10 +1,12 @@
-// When the user scrolls down 50px from the top of the document, resize the header's font size
+// When the user scrolls down 50px from the top of the document, resize the header's font size and change its content
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 50) {
         document.getElementById("header").style.fontSize = "30px";
+        document.getElementById("header").innerHTML = "Layla Ferreira";
     } else {
         document.getElementById("header").style.fontSize = "90px";
+        document.getElementById("header").innerHTML = "Layla <br> Ferreira";
     }
 }
